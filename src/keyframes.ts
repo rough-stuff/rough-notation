eb9248110a612299cd6cb8716c7a04ede06941bd
Preet Shihn <preetshihn@gmail.com>
2020-06-03 00:27:26 -0700

    .

--- src/keyframes.ts
+++ src/keyframes.ts
@@ -1,6 +1,6 @@
 export function ensureKeyframes() {
-  if (!(window as any).__rough_notation_keyframe_styles) {
-    const style = (window as any).__rough_notation_keyframe_styles = document.createElement('style');
+  if (!(window as any).__rno_kf_s) {
+    const style = (window as any).__rno_kf_s = document.createElement('style');
     style.textContent = `@keyframes rough-notation-dash { to { stroke-dashoffset: 0; } }`;
     document.head.appendChild(style);
   }
